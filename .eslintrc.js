8512ee80fa1afae71ec5e0bbeb703f49b92fc2b3
Jakub Biesiada <biesiadajakub@icloud.com>
2019-03-23 14:17:49 +0100

    Add config files

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+  ],
+  plugins: ['prettier', 'react-native'],
+  env: {
+    'react-native/react-native': true,
+  },
+  rules: {
+    'react/prop-types': 0,
+    'no-unused-vars': ['off'],
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 2,
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
---
+++ CHANGELOG.md
@@ -0,0 +1,1 @@
+# NativeColorCards Changelog
